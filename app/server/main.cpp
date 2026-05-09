3d2e349886d9221357ad3bf1159ce1c07735c989
kay30kim <kay30kim@gmail.com>
2026-05-08 17:29:36 -0700

    Accept multiple client

--- app/server/main.cpp
+++ app/server/main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <thread>
 
 #define PORT 8080
 
@@ -10,42 +11,41 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    // 1. socket 생성
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // 2. bind
     bind(server_fd, (struct sockaddr*)&address, sizeof(address));
 
-    // 3. listen
     listen(server_fd, 3);
     std::cout << "Server listening on port " << PORT << std::endl;
 
-    // 4. accept
-    client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-    std::cout << "Client connected!" << std::endl;
-
-    char buffer[1024] = {0};
-
     while (true) {
-        // 5. recv
-        int valread = read(client_socket, buffer, 1024);
-        if (valread <= 0) break;
+        client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        std::cout << "Client connected!" << std::endl;
 
-        std::cout << "Client: " << buffer << std::endl;
+        std::thread([client_socket]() {
+            char buffer[1024] = {0};
 
-        // 6. send reply
-        std::string reply = "Server received: ";
-        reply += buffer;
-        send(client_socket, reply.c_str(), reply.size(), 0);
+            while (true) {
+                int valread = read(client_socket, buffer, 1024);
+                if (valread <= 0) break;
 
-        memset(buffer, 0, sizeof(buffer));
+                std::cout << "Client: " << buffer << std::endl;
+
+                std::string reply = "Server received: ";
+                reply += buffer;
+                send(client_socket, reply.c_str(), reply.size(), 0);
+
+                memset(buffer, 0, sizeof(buffer));
+            }
+
+            close(client_socket);
+            std::cout << "Client disconnected!" << std::endl;
+        }).detach();
     }
 
-    close(client_socket);
     close(server_fd);
 }
-
